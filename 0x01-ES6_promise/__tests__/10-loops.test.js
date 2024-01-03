80375a22cd984ff09eeac76a20c75f8bdd4b3bf3
Niceblueman <wintheiser761@gmail.com>
2024-01-03 23:39:18 +0100

    remove testes

--- 0x01-ES6_promise/__tests__/10-loops.test.js
+++
@@ -1,8 +0,0 @@
-import appendToEachArrayValue from '../10-loops';
-
-describe('Task 10', () => {
-  it('should return the correct array after appending', () => {
-    const result = appendToEachArrayValue(['appended', 'fixed', 'displayed'], 'correctly-');
-    expect(result).toEqual(['correctly-appended', 'correctly-fixed', 'correctly-displayed']);
-  });
-});
